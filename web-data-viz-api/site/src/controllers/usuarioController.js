8c10415c95d08b64bcacf7b083e74a0c35a62a96
mariaeviegas <142421696+mariaeviegas@users.noreply.github.com>
2024-03-01 23:58:26 -0300

    Integração com API web-data-viz

---
+++ web-data-viz-api/site/src/controllers/usuarioController.js
@@ -0,0 +1,15 @@
+var usuarioModel = require("../models/usuarioModel");
+
+
+function logar() {
+    console.log("Controller do Login - Clonar a data viz separadamente e consultar o controller chamado Autenticar")
+}
+
+function cadastrar() {
+   console.log("Controller de Cadastro - Clonar a data viz separadamente e consultar o controller chamado Cadastrar")
+}
+
+module.exports = {
+    logar,
+    cadastrar
+}
