34d6490862a580206aed19c605b1549d36224515
agsousa4 <antonio.sousa@sptech.school>
2024-06-02 21:37:58 -0300

    Graficos da dashboard gestor finalizados

--- projeto/site/src/controllers/gestorController.js
+++ projeto/site/src/controllers/gestorController.js
@@ -175,11 +175,64 @@ function getDadosFuncionario(req, res) {
     }
 }
 
+function pegarDadosGrafico1(req, res){
+    let fkEmpresa = req.body.fkEmpresa;
+
+    console.log('Cheguei no controller')
+
+    if (fkEmpresa == undefined) {
+        res.status(400).send("Seu fkEmpresa está undefined!");
+    } else {
+        gestorModel.pegarDadosGrafico1(fkEmpresa)
+        .then(
+            function (resultado) {
+                if (resultado.length >= 1){
+                    res.json(resultado)
+                } else if (resultado.length == 0) {
+                    res.status(403).send("fkEmpresa inválido(s)");
+                }
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro)
+                res.status(500).json(erro.sqlMessage)
+            }
+        );
+    }
+}
+
+function pegarDadosGrafico2(req, res){
+    let fkEmpresa = req.body.fkEmpresa;
+
+
+    if (fkEmpresa == undefined) {
+        res.status(400).send("Seu fkEmpresa está undefined!");
+    } else {
+        gestorModel.pegarDadosGrafico2(fkEmpresa)
+        .then(
+            function (resultado) {
+                if (resultado.length >= 1){
+                    res.json(resultado)
+                } else if (resultado.length == 0) {
+                    res.status(403).send("fkEmpresa inválido(s)");
+                }
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro)
+                res.status(500).json(erro.sqlMessage)
+            }
+        );
+    }
+}
+
 module.exports = {
     gestorCadastrarUsuario,
     gestorCadastrarFuncionario,
     buscarInformacoesUsuario,
     editarInformacoesUsuario,
     excluirContaUsuario,
-    getDadosFuncionario
+    getDadosFuncionario,
+    pegarDadosGrafico1,
+    pegarDadosGrafico2
 }
